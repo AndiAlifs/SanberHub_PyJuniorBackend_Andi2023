af11103a7e4018ab9052dd6dadd663be38504aef
Andi Alifsyah <andyalyfsyah4@gmail.com>
2023-09-14 22:50:22 +0700

    model database define

---
+++ database.py
@@ -0,0 +1,39 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.engine import URL
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel
+
+uri = URL.create(
+    drivername="postgresql",
+    username="root",
+    password="root",
+    host="localhost",
+    port="5432",
+    database="sanberhub_pyjun",
+)
+
+engine = create_engine(uri)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+class Account(Base):
+    __tablename__ = "account"
+    nik = Column(String, primary_key=True)
+    nama = Column(String)
+    no_hp = Column(String)
+    no_rek = Column(String, nullable=True)
+    saldo = Column(Integer, default=0)
+
+class Transaksi(Base):
+    __tablename__ = "transaksi"
+    id = Column(Integer, primary_key=True)
+    no_rek = Column(String)
+    nominal = Column(Integer)
+    waktu = Column(DateTime)
+    kode_transaksi = Column(String)
+
+Base.metadata.create_all(engine)
+session.close()
